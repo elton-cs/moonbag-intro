941155ce45d25520795cbbac500f51d71e41ee5f
elton-cs <govada.elton@gmail.com>
2025-06-22 16:17:05 -0500

    remove old client

--- client/vite.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  plugins: [mkcert(), tailwindcss()],
-});
