978f79a1e4914dfa53b379b6cc236bd6bd5c53b0
zkelton <zkelton@zkeltons-MacBook-Pro.local>
2025-06-22 00:17:32 -0500

    feat: use tailwind

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [mkcert()],
+  plugins: [mkcert(), tailwindcss()],
 });
